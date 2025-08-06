036689d1df0e4b259caccb2ad9bbad56e0acaa44
JoelTobrix <pepedariojoel007@gmail.com>
2025-08-06 00:04:14 -0500

    Conexion Base de Datos

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,18 @@ Route::get('/page', function(){
     return view('options.pagina');
 })->name('pagina');
 
+/*
+Conexion a la Base de Datos
+|
+*/
+use Illuminate\Support\Facades\DB;
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "✅ Conexión a la base de datos exitosa";
+    } catch (\Exception $e) {
+        return "❌ Error en la conexión: " . $e->getMessage();
+    }
+});
 
