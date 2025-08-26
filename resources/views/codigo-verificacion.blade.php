61d02766f0c7e7852563c06958e75046958dfb27
JoelTobrix <pepedariojoel007@gmail.com>
2025-08-26 08:35:02 -0500

    Correo

--- resources/views/codigo-verificacion.blade.php
+++ resources/views/codigo-verificacion.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Verificar Código de Recuperación</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="icon" href="logo.png" type="image/png">
 </head>
 <body>
     <div class="container mt-5">
--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Recuperar Contraseña</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="icon" href="logo.png" type="image/png">
 </head>
 <body>
     <div class="container mt-5">
